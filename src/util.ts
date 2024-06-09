59326fddfd76eac03f5418a6ca44b5e8cc35d036
hackathononly <hackathononly@outlook.com>
2024-06-09 20:51:02 +0800

    create git action to fetch prayertimes

--- src/util.ts
+++ src/util.ts
@@ -1,86 +1,86 @@
-import MainData from "../src/prayertime/2024.json";
 import Constants from "./constants.js";
+import * as https from "https";
 
-export function downloadWaktuSolatByZone(zone: any) {
-  const baseURL =
-    "https://www.e-solat.gov.my/index.php?r=esolatApi/takwimsolat&period=year&zone=" +
-    zone;
-  return "hello";
+export function getFileName(dirname: any, zone: any) {
+  return `${dirname}/${zone}.json`;
+}
+
+export async function fetchDatas(url: any) {
+  return new Promise((resolve, reject) => {
+    https
+      .get(url, (res) => {
+        let body = "";
+        res.on("data", (chunk) => {
+          body += chunk;
+        });
+        res.on("end", () => {
+          try {
+            resolve(JSON.parse(body));
+          } catch (error) {
+            reject(error);
+          }
+        });
+      })
+      .on("error", (error) => {
+        reject(error);
+      });
+  }).then((response: any) => {
+    if (!("children" in response)) {
+      throw new Error("Invalid response.");
+    }
+    return response.children;
+  });
 }
 
 export function getZoneFromURL() {
+  // get full URL and just filter out zone
+
   const currentURL = new URL(window.location.href),
     pathname = currentURL.pathname,
     zone =
       pathname !== "/"
         ? decodeURI(pathname).split("/")[2].replace("/", "")
         : Constants.defaultSettings.zone;
+
   return zone;
 }
 
 export function getLocationFromZone(zone: any) {
+  // get array of location from passed zone, e.g. ['Pulau Aur']
+
   return Object.entries(Constants.locations).map(([key, value]: any) => {
     return Object.prototype.hasOwnProperty.call(value, zone) ? value[zone] : "";
   });
 }
 
 export function convertDateMonthDay(dateString: any) {
+  // return short date, e.g. Jun9
+
   let [day, month] = dateString.split("-");
 
   return `${month}${parseInt(day, 10)}`;
 }
 
-export function getWaktuSolat(zone: any) {
+export function getFormattedDate() {
+  // return date in full format, e.g. 09-Jun-2024
+
   const currentDate = new Date(),
     dd = String(currentDate.getDate()).padStart(2, "0"),
     month = currentDate.toLocaleString("default", { month: "short" }),
     year = currentDate.getFullYear(),
     formattedDate = `${dd}-${month}-${year}`;
 
-  const findPrayerTimeByZoneAndDate = (data: any, zone: any, date: string) => {
-    for (const row of data.rows) {
-      if (row.zone === zone) {
-        for (const prayerTime of row.prayerTime) {
-          if (prayerTime.date === date) {
-            return prayerTime;
-          }
-        }
-      }
-    }
-    return null;
-  };
-
-  interface CurrentCity {
-    zone: string;
-  }
-
-  const currentCity: CurrentCity = {
-    zone: zone || Constants.defaultSettings.zone,
-  };
-
-  interface MainDatas {
-    data: object;
-  }
-
-  const allWaktuSolat: MainDatas = {
-    data: MainData.data.data[0],
-  };
-
-  return findPrayerTimeByZoneAndDate(
-    allWaktuSolat.data,
-    currentCity.zone,
-    formattedDate,
-  );
+  return formattedDate;
 }
 
-export const timeString12hr = (time24: any) => {
+export function timeString12hr(time24: any) {
   return new Date("1970-01-01T" + time24 + "Z").toLocaleTimeString("en-US", {
     timeZone: "UTC",
     hour12: true,
     hour: "numeric",
     minute: "numeric",
   });
-};
+}
 
 export function getClosestPrayerTime(filteredObject: any) {
   const currentDate = new Date(),
