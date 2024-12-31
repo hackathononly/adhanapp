2636dd5ec9260287cf034a87ecc62be976b5cb79
hackathononly <hackathononly@outlook.com>
2025-01-01 01:53:10 +0800

    fix download prayertime script year folder

--- src/prayertimes/scripts/downloadPrayerTime.js
+++ src/prayertimes/scripts/downloadPrayerTime.js
@@ -23,7 +23,7 @@ for (const state in Constants.locations) {
       console.log(`Fetched ${prayertimes} prayertimes`);
 
       // const filename = getFileName("../2024", zone);
-      const filename = getFileName("./src/prayertimes/2024", zone);
+      const filename = getFileName("./src/prayertimes/2025", zone);
 
       // if (!fs.existsSync(filename)) {
       //     console.log("Creating a new prayertime cache file for", filename);
